6ada101854f3e18829ce1b5fe753415412b6332a
lef8 <alan.sipiorski14@gmail.com>
2023-08-22 19:42:15 +0200

    Include Question and QuestionOption entities in TypeOrmModule

--- src/quiz/quiz.module.ts
+++ src/quiz/quiz.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { QuizService } from './quiz.service';
 import { QuizResolver } from './quiz.resolver';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Quiz } from './models/quiz.entity';
+import { Question } from './models/question.entity';
+import { QuestionOption } from './models/question-option.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Quiz, Question, QuestionOption])],
   providers: [QuizService, QuizResolver],
 })
 export class QuizModule {}
