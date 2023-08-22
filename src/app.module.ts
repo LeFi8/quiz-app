525585df69026742aec1c1d43482de9c260ed234
lef8 <alan.sipiorski14@gmail.com>
2023-08-22 14:29:20 +0200

    Add required imports for graphql and typeorm, connect to docker db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuizModule } from './quiz/quiz.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [QuizModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'quiz_app_db',
+      entities: ['dist/**/*.entity.js'],
+      synchronize: true,
+    }),
+    QuizModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
