3b4aebabcc5b6e0f6f0f1d067acab3c272e53594
lef8 <alan.sipiorski14@gmail.com>
2023-08-23 15:00:43 +0200

    Inject quizRepository to QuizService

--- src/quiz/quiz.service.ts
+++ src/quiz/quiz.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quiz } from './models/quiz.entity';
+import { Repository } from 'typeorm';
 @Injectable()
-export class QuizService {}
+export class QuizService {
+  constructor(
+    @InjectRepository(Quiz) private quizRepository: Repository<Quiz>,
+  ) {}
+}
