b937a70ab131ef2f362e6a161efd63f10c26dcd2
lef8 <alan.sipiorski14@gmail.com>
2023-08-29 14:20:46 +0200

    Add validation to answer input

--- src/quiz/models/DTO/answer.dto.ts
+++ src/quiz/models/DTO/answer.dto.ts
@@ -1,8 +1,12 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { Length } from 'class-validator';
 
 @InputType()
 export class AnswerDto {
   @Field()
+  @Length(1, 50, {
+    message: 'Valid question has the length of 1-50 characters.',
+  })
   question: string;
 
   @Field({ nullable: true })
--- src/quiz/models/DTO/answer.input.ts
+++ src/quiz/models/DTO/answer.input.ts
@@ -1,11 +1,16 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { AnswerDto } from './answer.dto';
+import { ArrayNotEmpty, Length } from 'class-validator';
 
 @InputType()
 export class AnswerInput {
   @Field()
+  @Length(4, 50, {
+    message: 'Valid quiz name has between 4 and 50 characters. ',
+  })
   quizName: string;
 
   @Field(() => [AnswerDto])
+  @ArrayNotEmpty({ message: 'Cannot submit empty answers. ' })
   questionAnswers: AnswerDto[];
 }
